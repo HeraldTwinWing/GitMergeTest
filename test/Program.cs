d352af96c999f7edafa3aa6dde87347caf54a4ff
White Wing <Avalanche_Exia@Outlook.com>
2021-12-29 13:20:35 +0800

    br2 init

--- test/Program.cs
+++ test/Program.cs
@@ -4,14 +4,18 @@ namespace test
 {
     internal class Program
     {
-        public const int Branch = 1;
+        public const int Branch = 2;
         
         public static void Main(string[] args)
         {
             Console.Out.WriteLine($"Is this branch {Branch}?");
             if (Branch == 1)
             {
-                Console.Out.WriteLine("Yes, it's brach 1.");
+                Console.Out.WriteLine("Yes, it's branch 1.");
+            }
+            else
+            {
+                Console.Out.WriteLine("No, it isn't branch 1.");
             }
         }
     }
