d746d84ffcff126528d6065a9ddc6ef09217902e
White Wing <Avalanche_Exia@Outlook.com>
2021-12-29 13:16:46 +0800

    t

---
+++ test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace test
+{
+    internal class Program
+    {
+        public const int Branch = 1;
+        
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine($"Is this branch {Branch}?");
+            if (Branch == 1)
+            {
+                Console.Out.WriteLine("Yes, it's brach 1.");
+            }
+        }
+    }
+}
